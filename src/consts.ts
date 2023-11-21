307cfdd3822d6345ab8a4f01ea63f5e31af8f90d
btav <brian.ft.mit@gmail.com>
2023-11-20 22:01:49 -0500

    refactor: removed email and added spotify link

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 export const SITE_TITLE = "btav";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SPOTIFY_URL = "https://open.spotify.com/user/brian_ft";
 export const GITHUB_URL = "https://github.com/btav";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/brian-farias-tavares";
 export const TWITTER_URL = "https://twitter.com/braintavares";
 export const THREADS_URL = "https://www.threads.net/@brianfariastavares";
-export const MAILTO_EMAIL = "mailto:brian@btav.dev";

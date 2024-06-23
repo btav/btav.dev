268af429fede5cf6179840949ef1fcddbb1b1953
btav <brian.ft.mit@gmail.com>
2024-06-23 08:06:53 -0400

    removed threads

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,3 @@ export const SPOTIFY_URL = "https://open.spotify.com/user/brian_ft";
 export const GITHUB_URL = "https://github.com/btav";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/brian-farias-tavares";
 export const TWITTER_URL = "https://twitter.com/braintavares";
-export const THREADS_URL = "https://www.threads.net/@brianfariastavares";

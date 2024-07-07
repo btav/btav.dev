5c73ca9ff3014e94cb1b880caf462aa2078cee41
Brian Farias Tavares <brian.ft.mit@gmail.com>
2024-07-06 20:51:33 -0400

    feat: ui-changes (#7)

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,69 @@ export const SITE_DESCRIPTION = "Welcome to my website!";
 export const SPOTIFY_URL = "https://open.spotify.com/user/brian_ft";
 export const GITHUB_URL = "https://github.com/btav";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/brian-farias-tavares";
-export const TWITTER_URL = "https://twitter.com/braintavares";
+export const TWITTER_URL = "https://twitter.com/codingwithbrian";
+
+export const WORK = [
+  {
+    title: "Senior Software Engineer",
+    company: "Spotify",
+    startDate: "Jan 2023",
+    endDate: "Present",
+    image: "/spotify.png",
+    url: "https://spotify.com",
+  },
+  {
+    title: "Software Engineering Lead",
+    company: "Else Labs",
+    startDate: "Jan 2017",
+    endDate: "Dec 2022",
+    image: "/elselabs.png",
+    url: "https://elselabs.io",
+  },
+  {
+    title: "Software Developer",
+    company: "Versaterm",
+    startDate: "Apr 2012",
+    endDate: "Jan 2017",
+    image: "/versaterm.png",
+    url: "https://versaterm.com",
+  },
+  {
+    title: "Junior Services Specialist",
+    company: "Environment Canada",
+    startDate: "May 2011",
+    endDate: "Aug 2011",
+    image: "/environment-canada.png",
+    url: "https://weather.gc.ca/",
+  },
+];
+
+export const VOLUNTEER = [
+  {
+    title: "Board of Governors",
+    company: "CÉGEP Heritage College",
+    startDate: "Sept 2022",
+    endDate: "Present",
+    image: "/heritage.png",
+    url: "https://cegep-heritage.qc.ca",
+  },
+  {
+    title: "Organizer",
+    company: "ForwardJS Ottawa",
+    startDate: "Jul 2018",
+    endDate: "Present",
+    image: "/forward.png",
+    url: "https://www.meetup.com/ottawa-reactjs-meetup",
+  },
+];
+
+export const EDUCATION = [
+  {
+    title: "Computer Science",
+    company: "CÉGEP Heritage College",
+    startDate: "2010",
+    endDate: "2013",
+    image: "/heritage.png",
+    url: "https://www.cegep-heritage.qc.ca/future-students/programs/computer-science/home",
+  },
+];

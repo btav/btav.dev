f4bcc1c8691ac5e3a973b9e69685d3d688353cfb
btav <brian.ft.mit@gmail.com>
2024-11-11 11:33:09 -0500

    added bluesky link and about page

--- src/consts.ts
+++ src/consts.ts
@@ -11,6 +11,7 @@ export const SPOTIFY_URL = "https://open.spotify.com/user/brian_ft";
 export const GITHUB_URL = "https://github.com/btav";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/brian-farias-tavares";
 export const TWITTER_URL = "https://twitter.com/codingwithbrian";
+export const BLUESKY_URL = "https://bsky.app/profile/btav.dev";
 
 export const WORK = [
   {

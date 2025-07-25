295fffb56ac8cb4083a2b0f536d4910fc721a457
btav <brian.ft.mit@gmail.com>
2025-07-25 18:09:22 -0400

    remove resume page

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,4 @@
-import SpotifyImage from "./content/images/spotify.png";
-import ElseLabsImage from "./content/images/elselabs.png";
-import VersatermImage from "./content/images/versaterm.png";
-import EnvironmentCanadaImage from "./content/images/environment-canada.png";
-import HeritageImage from "./content/images/heritage.png";
-import ForwardImage from "./content/images/forward.png";
+
 
 export const SITE_TITLE = "Brian Farias Tavares";
 export const SITE_DESCRIPTION = "Welcome to my website!";
@@ -13,67 +8,4 @@ export const LINKEDIN_URL = "https://www.linkedin.com/in/brian-farias-tavares";
 export const TWITTER_URL = "https://twitter.com/codingwithbrian";
 export const BLUESKY_URL = "https://bsky.app/profile/btav.dev";
 
-export const WORK = [
-  {
-    title: "Senior Engineer",
-    company: "Spotify",
-    startDate: "Jan 2023",
-    endDate: "Present",
-    image: SpotifyImage,
-    url: "https://spotify.com",
-  },
-  {
-    title: "Software Engineering Lead",
-    company: "Else Labs",
-    startDate: "Jan 2017",
-    endDate: "Dec 2022",
-    image: ElseLabsImage,
-    url: "https://elselabs.io",
-  },
-  {
-    title: "Software Developer",
-    company: "Versaterm",
-    startDate: "Apr 2012",
-    endDate: "Jan 2017",
-    image: VersatermImage,
-    url: "https://versaterm.com",
-  },
-  {
-    title: "Junior Services Specialist",
-    company: "Environment Canada",
-    startDate: "May 2011",
-    endDate: "Aug 2011",
-    image: EnvironmentCanadaImage,
-    url: "https://weather.gc.ca/",
-  },
-];
 
-export const VOLUNTEER = [
-  {
-    title: "Organizer",
-    company: "ForwardJS Ottawa",
-    startDate: "Jul 2018",
-    endDate: "Present",
-    image: ForwardImage,
-    url: "https://www.meetup.com/ottawa-reactjs-meetup",
-  },
-  {
-    title: "Board of Governors",
-    company: "CÉGEP Heritage College",
-    startDate: "Sept 2022",
-    endDate: "June 2025",
-    image: HeritageImage,
-    url: "https://cegep-heritage.qc.ca",
-  },
-];
-
-export const EDUCATION = [
-  {
-    title: "Computer Science",
-    company: "CÉGEP Heritage College",
-    startDate: "2010",
-    endDate: "2013",
-    image: HeritageImage,
-    url: "https://www.cegep-heritage.qc.ca/future-students/programs/computer-science/home",
-  },
-];

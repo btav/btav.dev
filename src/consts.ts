fbf95cab67f5777d26ca2508dce8562105fbf480
btav <brian.ft.mit@gmail.com>
2025-08-23 11:56:46 -0400

    Remove extra blank lines from consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-
-
 export const SITE_TITLE = "Brian Farias Tavares";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const SPOTIFY_URL = "https://open.spotify.com/user/brian_ft";
@@ -7,5 +5,3 @@ export const GITHUB_URL = "https://github.com/btav";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/brian-farias-tavares";
 export const TWITTER_URL = "https://twitter.com/codingwithbrian";
 export const BLUESKY_URL = "https://bsky.app/profile/btav.dev";
-
-

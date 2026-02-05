6059cca7c3aad6a5be7371b9dd0c345dbaa2308f
btav <brian.ft.mit@gmail.com>
2026-02-05 08:33:38 -0500

    chore: redesign

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-export const SITE_TITLE = "Brian Farias Tavares";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const NAME = "brian farias tavares";
+export const DESCRIPTION = "software eng who cares for their craft.";
 export const SPOTIFY_URL = "https://open.spotify.com/user/brian_ft";
 export const GITHUB_URL = "https://github.com/btav";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/brian-farias-tavares";
